00a9811b4065807a91710142ad23bc9be971b07d
Celso-Luiz <45365462+Celso-Luiz@users.noreply.github.com>
2022-06-19 21:29:35 -0300

    modulo

---
+++ Modulos/main.js
@@ -0,0 +1,5 @@
+import { mostraCidade, mostraIdade, mostraHobby } from "./funcoes.js";
+
+console.log(mostraIdade("Celso", 28));
+console.log(mostraCidade("Celso", "Belo Horizonte"));
+console.log(mostraHobby("Celso", "Jogar"));
