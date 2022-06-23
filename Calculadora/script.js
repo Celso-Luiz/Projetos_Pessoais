525a69436fe7390f894d614377b0368ab7521431
Celso-Luiz <45365462+Celso-Luiz@users.noreply.github.com>
2022-06-23 17:55:16 -0300

    Update script.js

--- Calculadora/script.js
+++ Calculadora/script.js
@@ -1,9 +1,1 @@
-var num1 = inserto.onclick
-var operador = operador.onclick
-var num2 = inserto.onclick
-Var soma = 0
-Funcion operacao(num1, num2)
-    If (operador === +) {
-            soma = num1 + num2;
-        }Eles If ( operador === -){
-            Soma = num1 - num2;
+
